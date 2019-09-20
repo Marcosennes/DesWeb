2f0d4051e0ffb03713b7caa8e805f04fa5eedbc4
Marcosennes <43481166+Marcosennes@users.noreply.github.com>
2019-09-20 11:10:41 -0300

    Resolvendo JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(function () {
    $('[data-toggle="popover"]').popover();
 
    // $("#nome").addEventListener("blur", function() { nomeValidoFunction() }, false)
-   $("#id-form").submit(function(e) { validaTudoFunction(e) })
+   $("#cadastro-form").submit(validaTudoFunction(e));
 });
 
 function validaTudoFunction(e) {
@@ -14,10 +14,10 @@ function validaTudoFunction(e) {
 
    let nome_valido = nomeValidoFunction()
    let sexo_valido = sexoValidoFunction()
-   let passeio_valido = passeioValidoFunction()
-   let excursao_valida = excursaoValidaFunction()
+   let cidade_valida = cidadeValidoFunction()
+   let opcoes_validas = opcoesValidaFunction()
 
-   if (nome_valido && sexo_valido && passeio_valido && excursao_valida) {
+   if (nome_valido && sexo_valido && cidade_valida && opcoes_validas) {
       alert("Tudo Ok!")
       // $("#id-form").submit()
    }
@@ -26,47 +26,47 @@ function validaTudoFunction(e) {
    }
 }
 
-function excursaoValidaFunction() {
-   let ilha = $("#ilha")
-   let paraty = $("#paraty")
-   let buzios = $("#buzios")
+function opcoesValidaFunction() {
+   let email = $("#email")
+   let informacoesPessoais = $("#informacoesPessoais")
+   let salvar = $("#salvar")
 
-   let feedback_excursao = $("#feedback-excursao")
+   let feedback_opcoes = $("#feedback-opcoes")
 
-   let botoes = $("input.excursao:checked")
+   let botoes = $("input.opcoes:checked")
    if (botoes.length === 0) {
-      ilha.addClass("is-invalid")
-      ilha.removeClass("is-valid")
-      paraty.addClass("is-invalid")
-      paraty.removeClass("is-valid")
-      buzios.addClass("is-invalid")
-      buzios.removeClass("is-valid")
-      feedback_excursao.addClass("d-block")
+      email.addClass("is-invalid")
+      email.removeClass("is-valid")
+      informacoesPessoais.addClass("is-invalid")
+      informacoesPessoais.removeClass("is-valid")
+      salvar.addClass("is-invalid")
+      salvar.removeClass("is-valid")
+      feedback_opcoes.addClass("d-block")
       return false
    }
    else {
-      ilha.addClass("is-valid")
-      ilha.removeClass("is-invalid")
-      paraty.addClass("is-valid")
-      paraty.removeClass("is-invalid")
-      buzios.addClass("is-valid")
-      buzios.removeClass("is-invalid")
-      feedback_excursao.removeClass("d-block")
+      email.addClass("is-valid")
+      email.removeClass("is-invalid")
+      informacoesPessoais.addClass("is-valid")
+      informacoesPessoais.removeClass("is-invalid")
+      salvar.addClass("is-valid")
+      salvar.removeClass("is-invalid")
+      feedback_opcoes.removeClass("d-block")
       return true
    }
 }
 
-function passeioValidoFunction() {
-   let passeio = $("#passeio")
+function cidadeValidoFunction() {
+   let cidade = $("#cidade")
 
-   if (passeio.val() === '') {
-      passeio.addClass("is-invalid")
-      passeio.removeClass("is-valid")
+   if (cidade.val() === '') {
+      cidade.addClass("is-invalid")
+      cidade.removeClass("is-valid")
       return false
    }
    else {
-      passeio.removeClass("is-invalid")
-      passeio.addClass("is-valid")
+      cidade.removeClass("is-invalid")
+      cidade.addClass("is-valid")
       return true
    }
 }
